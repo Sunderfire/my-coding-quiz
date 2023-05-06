ed1eb319bdee0fea2b4f55f14026a0f35e5ae319
Griffin Pelletier <umbral.lights@live.com>
2023-05-06 14:10:01 -0400

    Initial HTML, CSS, and README template

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+var questionText = document.querySelector(".question");
+var instructionText = document.querySelector(".start-instructions");
+var questionButtons = document.querySelector(".question-buttons");
+var startButton = document.querySelector(".start-button");
+var resultSpan = document.querySelector("result-declaration");
+
+
+
+
+//When I click the start button the the user is presented a random coding question
+//When I answer the question, I am presented with another question, five times
+//When I answer a question incorrectly, time is subtracted from the clock
+//Then when all questions have been answered or the timer reached zero
+    //The game ends
+//When the game ends, I can save my initials and my score.
